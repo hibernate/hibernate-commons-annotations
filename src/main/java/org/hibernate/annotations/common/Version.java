b8a0befc5430f6d859e1dd7d0aba58c61f9ebb01
Emmanuel Bernard <emmanuel@hibernate.org>
2010-03-09 16:50:29 +0000

    new version
    git-svn-id: https://svn.jboss.org/repos/hibernate/commons-annotations/trunk@18946 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/main/java/org/hibernate/annotations/common/Version.java
+++ src/main/java/org/hibernate/annotations/common/Version.java
@@ -30,7 +30,7 @@ import org.slf4j.LoggerFactory;
  * @author Emmanuel Bernard
  */
 public class Version {
-	public static final String VERSION = "3.2.0.Final";
+	public static final String VERSION = "3.2.1-SNAPSHOT";
 	private static Logger log = LoggerFactory.getLogger( Version.class );
 
 	static {
