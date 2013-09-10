61a32bcc1f3b5d27adabf57525fa565a2b28e402
Steve Ebersole <steve@hibernate.org>
2013-09-10 14:58:02 -0500

    HCANN-58 - Migrate build to Gradle

--- src/main/java/org/hibernate/annotations/common/Version.java
+++ src/main/java/org/hibernate/annotations/common/Version.java
@@ -1,10 +1,10 @@
 /*
  * Hibernate, Relational Persistence for Idiomatic Java
  *
- * Copyright (c) 2008-2011, Red Hat Middleware LLC or third-party contributors as
+ * Copyright (c) 2008-2013, Red Hat Inc. or third-party contributors as
  * indicated by the @author tags or express copyright attribution
  * statements applied by the authors.  All third-party contributions are
- * distributed under license by Red Hat Middleware LLC.
+ * distributed under license by Red Hat Inc.
  *
  * This copyrighted material is made available to anyone wishing to use, modify,
  * copy, or redistribute it subject to the terms and conditions of the GNU
@@ -23,20 +23,10 @@
  */
 package org.hibernate.annotations.common;
 
-import org.hibernate.annotations.common.util.impl.Log;
-import org.hibernate.annotations.common.util.impl.LoggerFactory;
-
 /**
  * @author Emmanuel Bernard
  */
 public class Version {
-
-//	private static final Log log = LoggerFactory.make( Version.class.getName() );
-//
-//	static {
-//		log.version( getVersionString() );
-//	}
-
 	public static String getVersionString() {
 		return "[WORKING]";
 	}
