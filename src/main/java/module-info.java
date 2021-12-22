3e7622a75aa894197978ee276442509a22452b8f
Sanne Grinovero <sanne@hibernate.org>
2021-12-22 13:14:40 +0000

    HCANN-118 Introduce a real module-info.java file

---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module org.hibernate.commons.annotations {
+	exports org.hibernate.annotations.common.annotationfactory;
+	exports org.hibernate.annotations.common.reflection;
+	exports org.hibernate.annotations.common.reflection.java;
+}
